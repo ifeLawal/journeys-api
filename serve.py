31d98b8dfc36a88720fc5908bebf9d07b1238761
Ife <il693@nyu.edu>
2021-06-09 00:38:05 -0400

    reduced file size and prepped for procfile

--- serve.py
+++ serve.py
@@ -1,12 +1,21 @@
 """Code for a flask API to Create, Read, Update, Delete users"""
-import argparse
+import os
 
 from journeys import create_app
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--port", help="Port for debug server to listen on", default=4000)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--port", help="Port for debug server to listen on", default=4000)
+# args = parser.parse_args()
 
+debug = True
+threaded = True
+host = "127.0.0.1"
+port = int(os.environ.get("PORT", 5000))
+if os.environ.get("PORT"):
+    host = "0.0.0.0"
+    debug = False
+    threaded = False
+    
 app = create_app()
 
-app.run(debug=True, threaded=True, host="127.0.0.1", port=args.port)
+app.run(debug=debug, threaded=threaded, host=host, port=port)
